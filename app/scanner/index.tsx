3e1e5dfac16be951496fbcb14ef130561705f6b9
ram4444 <ramwt4444@gmail.com>
2024-11-09 21:15:26 +0000

    QR scanner implementation

--- app/scanner/index.tsx
+++ app/scanner/index.tsx
@@ -10,10 +10,98 @@ import {
 } from "react-native";
 import { Overlay } from "./Overlay";
 import { useEffect, useRef } from "react";
+import { useSDK } from '@metamask/sdk-react';
+import Web3 from 'web3';
 
-export default function Home() {
+export type Props = {
+  navigation: any,
+};
+
+export default function Home(navigation) {
   const qrLock = useRef(false);
   const appState = useRef(AppState.currentState);
+  // ---------------State variables--------------- 
+
+  const {
+    sdk,
+    provider: ethereum,
+    status,
+    chainId,
+    account,
+    balance,
+    readOnlyCalls,
+    connected,
+  } = useSDK();
+
+  // ---------------Style Sheets---------------
+
+  // ---------------Visual Items--------------- 
+
+  // ---------------onPress Handler---------------
+
+  // ---------------navigate function---------------
+  const eth_estimationGas = async (to, value) => {
+    try {
+      const result = await ethereum?.request({
+        method: 'eth_estimateGas',
+        params: [
+          {
+            from: account,
+            to: to,
+            value: value
+          }
+        ]
+      });
+      console.log('eth_sendTransaction', result);
+      return result;
+    } catch (e) {
+      console.log('ERROR', e);
+    }
+  }
+
+  const eth_gasPrice = async () => {
+    try {
+      const result = await ethereum?.request({
+        method: 'eth_gasPrice',
+        params: [
+        ]
+      });
+      console.log('eth_sendTransaction', result);
+      return result;
+    } catch (e) {
+      console.log('ERROR', e);
+    }
+  }
+
+  const sendTransaction = async (qr:any) => {
+    try {
+        const value = Web3.utils.toWei(qr.crypto_price_ezread, 'ether')
+        const gas = eth_estimationGas(qr.toWalletAddr, value)
+        const gasPrice = eth_gasPrice()
+        console.log('value', value);
+        console.log('gas', gas);
+        console.log('gasPrice', gasPrice);
+
+        /*
+        const result = await ethereum?.request({
+          method: 'eth_sendTransaction',
+          params: [
+            {
+              to: qr.toWalletAddr,
+              from: account,
+              gas: gas,
+              value: value,
+              gasPrice: gasPrice
+            }
+          ]
+        });
+        console.log('eth_sendTransaction', result);
+        */
+        //setResponse(result);
+    } catch (e) {
+      console.log('ERROR', e);
+    }
+  };
 
   useEffect(() => {
     const subscription = AppState.addEventListener("change", (nextAppState) => {
