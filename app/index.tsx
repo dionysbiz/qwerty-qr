75d9c363d33bcaf283a11c5b932f6c476062029a
ram4444 <ramwt4444@gmail.com>
2024-12-22 02:52:16 +0000

    proj files update

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,13 @@ import { Text, View } from "react-native";
 var Buffer = require('buffer/').Buffer  // note: the trailing slash is important!
 import App from './App'
 
+import 'react-native-get-random-values';
+import 'react-native-url-polyfill/auto';
+import { ReadableStream } from 'web-streams-polyfill';
+if (typeof global.ReadableStream === 'undefined') {
+    global.ReadableStream = ReadableStream;
+}
+
 export default function Index() {
   return (
     <App />
--- metro.config.js
+++ metro.config.js
@@ -6,7 +6,22 @@ const config = getDefaultConfig(__dirname);
 
 config.resolver.extraNodeModules = {
     ...require('node-libs-expo'),
+    ...require('node-libs-react-native'),
+    //...require('react-native-tcp-socket'),
     crypto: require.resolve('crypto-browserify'),
+    //net: require.resolve('node-libs-react-native/mock/net'),
+    net: require.resolve('react-native-tcp-socket'),
+    tls: require.resolve('react-native-tcp-socket'),
+    //tls: require.resolve('node-libs-react-native/mock/tls'),
+    // Try put this to node modules
+    /*
+    import TcpSockets from 'react-native-tcp-socket';
+
+    export const Server = TcpSockets.TLSServer;
+    export const TLSSocket = TcpSockets.TLSSocket;
+    export const connect = TcpSockets.connectTLS;
+    export const createServer = TcpSockets.createTLSServer;
+    */
 };
 
 config.resolver.unstable_enablePackageExports= true,
