76eab8f0e81652edb4b441a8623666d23da1fed2
ram4444 <ramwt4444@gmail.com>
2025-01-06 19:10:41 +0000

    Ready to put QR orders to SCDF kafka

--- app/scanner/index.tsx
+++ app/scanner/index.tsx
@@ -20,6 +20,8 @@ import Web3 from 'web3';
 import { triggerTransactionv2 } from '../src/utils/ethUtil';
 import { putQrorder, scanQROrders } from '../src/utils/awsClient';
 
+import { url } from '../src/properties/urls_local'
+
 var hash = require('object-hash');
 
 export type Props = {
@@ -76,13 +78,69 @@ export default function Home(navigation) {
     }
   };
 
-  const onTransactionSuccess = async() => {
+  const onTransactionSuccess = async(receipt) => {
     qrLock.current = false;
     setWaitingModalVisible(false)
+    console.log("ON Success after coming back from Metamask")
+    console.log("TxHash:")
+    console.log(receipt.transactionHash)
+    // send qrData And receipt to Kafka with topic
+    const topicName = 'qrItemOrder-'+qrData.toWalletAddr
+    const orderTime = new Date()
+
+    // This is for putting to AWS dynamoDB
+    const qrorderItem = {
+      chainId: { S: qrData.crypto_chain_id },
+      itemName: { S: qrData.name},
+      fromAddr: { S: walletAddr },
+      toAddr: { S: qrData.toWalletAddr },
+      cryptoNameShort: {S: qrData.crypto_name_short },
+      cryptoPriceEzread: { S: qrData.crypto_price_ezread },
+      cryptoContractAddr: {S: qrData.crypto_contract_addr },
+      transactionHash: {S: receipt.transactionHash},
+      createDate: {S: orderTime}
+  }
+
+    fetch(url.kafka_publisher, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        /* receive side JSONparse
+        order.id,
+        itemList.length,//?
+        order.name,
+        order.crypto_name_short,
+        order.crypto_contract_addr,
+        order.crypto_chain_id,
+        order.crypto_price_ezread,
+        order.dateCreate,
+        order.fromAddr,
+        order.txHash
+        */
+        id: hash(qrorderItem),
+        name: qrData.name,
+        crypto_name_short: qrData.crypto_name_short,
+        crypto_contract_addr: qrData.crypto_contract_addr,
+        crypto_chain_id: qrData.crypto_chain_id,
+        crypto_price_ezread: qrData.crypto_price_ezread,
+        dateCreate: orderTime,
+        fromAddr: account,
+        txHash: receipt.transactionHash,
+      }),
+    }
+    ).then((response) => {
+      console.log("Response from SCDF kafkapublisher feign client")
+      console.log(response);
+    })
+    
   }
 
   const onTransactionFail = async() => {
     console.log("Transaction Fail")
+    console.log("ON Fail after coming back from Metamask")
     //setTimeout(async () => {
       qrLock.current = false;
       setWaitingModalVisible(false)
@@ -107,7 +165,9 @@ export default function Home(navigation) {
     setTimeout(async () => {
       
       if (qrData.crypto_price_ezread==='0') {
+        
         // "order_id, chainId, fromAddr, toAddr, product_name, description, currencyName, product_price, transactionHash",
+        // Putting order to AWS dynamoDB
         const qrorderItem = {
             chainId: { S: qrData.crypto_chain_id },
             itemName: { S: qrData.name},
@@ -121,7 +181,42 @@ export default function Home(navigation) {
         }
         const order_id = hash(qrorderItem)
         qrorderItem.order_id = {S: order_id}
-        putQrorder(qrorderItem)
+        //putQrorder(qrorderItem)
+        
+        // putting order to 
+        fetch(url.kafka_publisher, {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            /* receive side JSONparse
+            order.id,
+            itemList.length,//?
+            order.name,
+            order.crypto_name_short,
+            order.crypto_contract_addr,
+            order.crypto_chain_id,
+            order.crypto_price_ezread,
+            order.dateCreate,
+            order.fromAddr,
+            order.txHash
+            */
+            id: hash(qrorderItem),
+            name: qrData.name,
+            crypto_name_short: qrData.crypto_name_short,
+            crypto_contract_addr: qrData.crypto_contract_addr,
+            crypto_chain_id: qrData.crypto_chain_id,
+            crypto_price_ezread: qrData.crypto_price_ezread,
+            dateCreate: new Date(),
+            fromAddr: account,
+            txHash: "0x5e5c7a8610d5b420f77f755924264d89694513bded62762de2bd41d967bd5b30",
+          }),
+        }).then((response) => {
+          console.log("Response from SCDF kafkapublisher feign client")
+          console.log(response);
+        })
         
         setConfirmModalVisible(false)
         setWaitingModalVisible(true)
@@ -136,8 +231,8 @@ export default function Home(navigation) {
           walletAddr, 
           qrData.toWalletAddr, 
           qrData.crypto_price_ezread, 
-          onTransactionSuccess(),
-          onTransactionFail())
+          onTransactionSuccess,
+          onTransactionFail)
         setConfirmModalVisible(false)
         setWaitingModalVisible(true)
       }
