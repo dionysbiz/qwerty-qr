2d2809c29f840577e58a87afe786da8d5bba0ab4
Ram Lau <ramwt4444@gmail.com>
2025-03-31 04:39:02 +0100

    hardcode the url which needs enchancement

--- app/src/components/apollo-client.tsx
+++ app/src/components/apollo-client.tsx
@@ -5,21 +5,38 @@ import { createClient } from 'graphql-ws';
 
 import { getMainDefinition } from '@apollo/client/utilities';
 
-import { url } from '../properties/urls_local'
+import { url_local } from '../properties/urls_local'
+import { url_dev } from '../properties/urls_dev'
+
+import DeviceInfo from 'react-native-device-info'
+
+var url:any = ""
+
+DeviceInfo.isEmulator().then((isEmulator) => {
+  if (isEmulator) {
+    //url = url_local;
+    url = url_dev;
+  } else {
+    url = url_dev;
+  }
+});
 
 // HTTP link for regular queries and mutations
 
 //kubectl port-forward service/kafkaconsumergraphqlsub-dev 8080:8080 -n kafkaconsumergraphqlsub-dev
 const httpLink = new HttpLink({
-  uri: url.graphql, // Your GraphQL server's HTTP URL
+  //uri: url.graphql, // Your GraphQL server's HTTP URL
+  uri: 'http://kafkaconsumergraphqlsub-dev.dionys.xyz/graphql',
   //uri: 'http://10.0.2.2:8080/graphql',
 });
 
 // WebSocket link for subscriptions
 
 const wsLink = new GraphQLWsLink(createClient({
-  url: url.ws_graphqlSubscription,
+  //url: url.ws_graphqlSubscription,
   //url: 'ws://10.0.2.2:8080/subscriptions',
+  url: 'ws://kafkaconsumergraphqlsub-dev.dionys.xyz/subscriptions',
+  keepAlive: 10000, // Sends a keep-alive message every 10 seconds
 }));
 
 
